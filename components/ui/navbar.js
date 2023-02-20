2dae5295362542951116acb96df5a993ae145e60
PauliusTaraskevicius <fixas1029@gmail.com>
2023-02-20 23:25:46 +0200

    productionv8

--- components/ui/navbar.js
+++ components/ui/navbar.js
@@ -28,7 +28,7 @@ function Navbar() {
                         viewBox="0 0 24 24"
                         stroke-width="1.5"
                         stroke="currentColor"
-                        class="w-6 h-6"
+                        class="w-6 h-6" 
                       >
                         <path
                           stroke-linecap="round"
