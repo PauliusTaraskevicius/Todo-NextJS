de24a7a9be691823e206d77635d8deec46e8a113
PauliusTaraskevicius <fixas1029@gmail.com>
2023-02-22 21:19:26 +0200

    user can only view it's own tasks

--- components/Posts/AllTasks.js
+++ components/Posts/AllTasks.js
@@ -8,36 +8,30 @@ function TasksList({ feed }) {
 
   return (
     <div>
-      {feed.length > 0 ? (
-        feed.map((item, index) => (
-          <div key={index}>
-            {session.user.email === item.author.email ? (
-              <div className="flex mb-12 items-center border-b border-gray-200">
-                <p
-                  className={
-                    item.completed
-                      ? "w-full text-grey-darkest break-all line-through"
-                      : "w-full text-grey-darkest break-all "
-                  }
-                >
-                  {item.text}
-                  <br />
-                </p>
-                <div className="flex mb-2">
-                  <CompletionBtn completion={item.completed} id={item.id} />
-                  <DeleteBtn id={item.id} />
+      {feed.length > 0
+        ? feed.map((item, index) => (
+            <div key={index}>
+              {session.user.email === item.author.email ? (
+                <div className="flex mb-12 items-center border-b border-gray-200">
+                  <p
+                    className={
+                      item.completed
+                        ? "w-full text-grey-darkest break-all line-through"
+                        : "w-full text-grey-darkest break-all "
+                    }
+                  >
+                    {item.text}
+                    <br />
+                  </p>
+                  <div className="flex mb-2">
+                    <CompletionBtn completion={item.completed} id={item.id} />
+                    <DeleteBtn id={item.id} />
+                  </div>
                 </div>
-              </div>
-            ) : (
-              <p>No posts found.</p>
-            )}
-          </div>
-        ))
-      ) : (
-        <div>
-          <p>No posts found.</p>
-        </div>
-      )}
+              ) : null}
+            </div>
+          ))
+        : null}
     </div>
   );
 }
