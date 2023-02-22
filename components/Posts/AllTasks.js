fb7b74893a375c3b06c285978a79138a3b3501b4
PauliusTaraskevicius <fixas1029@gmail.com>
2023-02-22 21:08:23 +0200

    user can only view it's own tasks

--- components/Posts/AllTasks.js
+++ components/Posts/AllTasks.js
@@ -1,28 +1,39 @@
+import { useSession } from "next-auth/react";
+
 import DeleteBtn from "../Buttons/DeleteBtn";
 import CompletionBtn from "../Buttons/CompletionBtn";
 
 function TasksList({ feed }) {
+  const { data: session, status } = useSession();
+
   return (
     <div>
       {feed.length > 0 ? (
         feed.map((item, index) => (
-          <div
-            className="flex mb-12 items-center border-b border-gray-200"
-            key={index}
-          >
-            <p
-              className={
-                item.completed
-                  ? "w-full text-grey-darkest break-all line-through"
-                  : "w-full text-grey-darkest break-all "
-              }
-            >
-              {item.text}
-            </p>
-            <div className="flex mb-2">
-              <CompletionBtn completion={item.completed} id={item.id} />
-              <DeleteBtn id={item.id} />
-            </div>
+          <div>
+            {session.user.email === item.author.email ? (
+              <div
+                className="flex mb-12 items-center border-b border-gray-200"
+                key={index}
+              >
+                <p
+                  className={
+                    item.completed
+                      ? "w-full text-grey-darkest break-all line-through"
+                      : "w-full text-grey-darkest break-all "
+                  }
+                >
+                  {item.text}
+                  <br />
+                </p>
+                <div className="flex mb-2">
+                  <CompletionBtn completion={item.completed} id={item.id} />
+                  <DeleteBtn id={item.id} />
+                </div>
+              </div>
+            ) : (
+              <p>No posts found.</p>
+            )}
           </div>
         ))
       ) : (
